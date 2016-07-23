cca93c116033e1aef16ab04256e7274eacd87d61
nicosalvato <nicosalvato@gmail.com>
2016-07-23 22:09:39 +0300

    Added HAL renderers for Item but still not working

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,8 @@
 // Place your Spring DSL code here
+import grails.rest.render.hal.*
+import et.wolisso_inventory.Item
+
 beans = {
+    halItemRenderer(HalJsonRenderer, et.wolisso_inventory.Item)
+    halItemCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.Item)
 }
