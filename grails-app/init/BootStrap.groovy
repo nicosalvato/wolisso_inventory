b3cf29abd1fab6accb8c9c92cf5f7a3da476bba4
nicosalvato <nicosalvato@gmail.com>
2016-07-29 00:09:31 +0300

    Adjusted Bootstrap

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,5 +1,6 @@
 import et.wolisso_inventory.Item
 import et.wolisso_inventory.Report
+import et.wolisso_inventory.Maintenance
 
 class BootStrap {
 
@@ -11,7 +12,10 @@ class BootStrap {
 		    			code: "CODE$it",
 		    			externalCode: "externalCode$it",
 		    			name: "Item $it",
-		    			description: "Description for item $it"
+		    			description: "Description for item $it",
+                        price: it % 3 * 250.0,
+                        isDonation: it % 3 == 0,
+                        deliveryDate: new Date(2016 - 1900 - it, it, 28 % it)
 		    		).save flush: true	
 		    	}
                 def categories = ['OUT_OF_SERVICE', 'CONSUMABLE_MISSING', 'REPARING', 'REPAIRED']
@@ -19,13 +23,20 @@ class BootStrap {
                     new Report(
                         item: Item.load(it % 5 + 1),
                         category: categories[it % 4]
-                    ).save flush: true
+                    ).save()
+
+                    new Maintenance(
+                        item: Item.load(it % 5 + 1),
+                        cost: it * 230.0,
+                        maintenanceDate: new Date(2016 - 1900, it, 28 % it)
+                    ).save()
                 }
     		}
             test {
                 new Item(
                     code: "AAA",
-                    name: "Aaa"
+                    name: "Aaa",
+                    price: it * 100.0
                 ).save flush: true  
             }
     	}
