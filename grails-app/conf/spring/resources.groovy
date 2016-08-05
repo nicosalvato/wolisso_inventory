b019a819f2fde662702192c9d17297e0af114806
nicosalvato <nicosalvato@gmail.com>
2016-08-05 22:12:33 +0300

    Added default HAL support for every resource

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,14 +3,23 @@ import grails.rest.render.hal.*
 import et.wolisso_inventory.Item
 import et.wolisso_inventory.Report
 import et.wolisso_inventory.Maintenance
+import et.wolisso_inventory.MaintenanceProvider
+import et.wolisso_inventory.Manufacturer
+import et.wolisso_inventory.Location
 import et.wolisso_inventory.enums.ItemStatus
 
 beans = {
-	// HAL renderers, they kick in when Accept header in the request is set to json+hal
+	// HAL renderers, they kick in when 'Accept' header in the request is set to 'application/json+hal'
     halItemRenderer(HalJsonRenderer, et.wolisso_inventory.Item)
     halItemCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.Item)
 	halReportRenderer(HalJsonRenderer, et.wolisso_inventory.Report)
     halReportCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.Report)
 	halMaintenanceRenderer(HalJsonRenderer, et.wolisso_inventory.Maintenance)
     halMaintenanceCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.Maintenance)
+    halMaintenanceProviderRenderer(HalJsonRenderer, et.wolisso_inventory.MaintenanceProvider)
+    halMaintenanceProviderCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.MaintenanceProvider)
+    halManufacturerRenderer(HalJsonRenderer, et.wolisso_inventory.MaintenanceProvider)
+    halManufacturerCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.MaintenanceProvider)
+    halLocationRenderer(HalJsonRenderer, et.wolisso_inventory.Location)
+    halLocationCollectionRenderer(HalJsonCollectionRenderer, et.wolisso_inventory.Location)
 }
